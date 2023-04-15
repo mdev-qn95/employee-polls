2c36db209e6cb393799f3980018ffa6bc6d8cdc2
Thái Văn Phú <thaivanphu.qn95@gmail.com>
2023-04-15 23:43:15 +0700

    Add react-bootstrap and create UI for I for Login Page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,67 @@
+import React from "react";
+import { useState } from "react";
+import { Container, Row, Col, Form, Button, Image } from "react-bootstrap";
+import login from "../images/login.png";
+
 const Login = () => {
+  const [user, setUser] = useState("zoshikanlu");
+  const [password, setPassword] = useState("pass246");
+
+  const userOnChangeHandle = (e) => {
+    const value = e.target.value;
+    setUser(value);
+  };
+
+  const passwordOnChangeHandle = (e) => {
+    const value = e.target.value;
+    setPassword(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUser("");
+    setPassword("");
+  };
   return (
-    <div>
-      <span>LOGIN</span>
-    </div>
+    <Container className="mt-5">
+      <Row>
+        <Col>
+          <Image src={login} width="400px" alt="Login" />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h2>Log In</h2>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="user">
+              <Form.Label>User</Form.Label>
+              <Form.Control
+                type="text"
+                name="user"
+                placeholder="User"
+                value={user}
+                onChange={userOnChangeHandle}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={password}
+                onChange={passwordOnChangeHandle}
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit" className="mt-5">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
