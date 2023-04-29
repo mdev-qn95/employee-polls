78d800a8aad5c68746fb28a29613b3b4ac030975
Thái Văn Phú <thaivanphu.qn95@gmail.com>
2023-04-29 20:13:38 +0700

    Create UI for New Poll

--- src/components/New.js
+++ src/components/New.js
@@ -1,8 +1,60 @@
+import { useState } from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+
 const New = () => {
+  const [firstVal, setFirstVal] = useState("");
+  const [secondVal, setSecondVal] = useState("");
+
+  const firstOptionChangeHandle = (e) => {
+    const value = e.target.value;
+    setFirstVal(value);
+  };
+
+  const secondOptionChangeHandle = (e) => {
+    const value = e.target.value;
+    setSecondVal(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <span>NEW</span>
-    </div>
+    <Container className="mt-5 new">
+      <Row>
+        <Col>
+          <h2>Would You Rather</h2>
+          <span className="gray">Create Your Own Poll</span>
+          <Form onSubmit={handleSubmit} className="mt-5">
+            <Form.Group controlId="user">
+              <Form.Label className="bold">First Option</Form.Label>
+              <Form.Control
+                type="text"
+                name="one"
+                placeholder="Option One"
+                value={firstVal}
+                onChange={firstOptionChangeHandle}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="password" className="mt-5">
+              <Form.Label className="bold">Second Option</Form.Label>
+              <Form.Control
+                type="text"
+                name="two"
+                placeholder="Option Two"
+                value={secondVal}
+                onChange={secondOptionChangeHandle}
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit" className="mt-5">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
