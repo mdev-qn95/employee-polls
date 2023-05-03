848ca24b5ed01a1d7c5d22a735555a7c17176535
Thái Văn Phú <thaivanphu.qn95@gmail.com>
2023-05-03 11:15:13 +0700

    Fix comment round 2

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,43 @@
 import { connect } from "react-redux";
 import Card from "./Card";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 
 const Home = ({ authed, questions }) => {
   return (
     <div className="home" data-testid="home-page">
-      <div className="questions">
-        <div className="title">UnAnswered</div>
-        <div className="list-card">
-          {questions
-            .filter(
-              (question) =>
-                !question.optionOne.votes.includes(authed) &&
-                !question.optionTwo.votes.includes(authed)
-            )
-            .map((question) => (
-              <Card key={question.id} question={question}></Card>
-            ))}
-        </div>
-      </div>
-      <div className="questions">
-        <div className="title">Answered</div>
-        <div className="list-card">
-          {questions
-            .filter(
-              (question) =>
-                question.optionOne.votes.includes(authed) ||
-                question.optionTwo.votes.includes(authed)
-            )
-            .map((question) => (
-              <Card key={question.id} question={question}></Card>
-            ))}
-        </div>
-      </div>
+      <Tabs defaultActiveKey="unAnswered" className="mb-3">
+        <Tab eventKey="unAnswered" title="UnAnswered">
+          <div className="questions">
+            <div className="list-card">
+              {questions
+                .filter(
+                  (question) =>
+                    !question.optionOne.votes.includes(authed) &&
+                    !question.optionTwo.votes.includes(authed)
+                )
+                .map((question) => (
+                  <Card key={question.id} question={question}></Card>
+                ))}
+            </div>
+          </div>
+        </Tab>
+        <Tab eventKey="answered" title="Answered">
+          <div className="questions">
+            <div className="list-card">
+              {questions
+                .filter(
+                  (question) =>
+                    question.optionOne.votes.includes(authed) ||
+                    question.optionTwo.votes.includes(authed)
+                )
+                .map((question) => (
+                  <Card key={question.id} question={question}></Card>
+                ))}
+            </div>
+          </div>
+        </Tab>
+      </Tabs>
     </div>
   );
 };
